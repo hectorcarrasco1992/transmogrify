5081fed805ae128452b85efbcbf1a41f0fbccec5
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-11-21 13:42:08 -0500

    finished

--- main.js
+++ main.js
@@ -1,20 +1,55 @@
 const biggify = function(num) {
+  return num + 9000
   
 }
 
 const nasafy = function(num) {
-  
+  const arr = []
+  let count = num
+
+  while (count >= 1){
+    arr.push(count);
+    count--;
+  }
+
+  arr.push('Blast off!')
+
+  return arr
 }
 
 const reversify = function(str) {
+  return str.split('').reverse().join('')
   
 }
 
 const titleify = function(str) {
-  
+  let titleCasedWords = [];
+  for (const word of str.split(' ')) {
+    titleCasedWords.push(word[0].toUpperCase() + word.slice(1).toLowerCase());
+  }
+
+  return titleCasedWords.join(' ');
 }
+  
+  
+  
+
 
 const crazify = function(str) {
+  let newStr = '';
+  let crazy = 0;
+  for (const char of str) {
+    if (char === ' ') {
+      newStr+= ' ';
+    } else {
+      newStr+= crazy % 2 === 1
+        ? char.toUpperCase()
+        : char.toLowerCase();
+      crazy++;
+    }
+  }
+
+  return newStr
   
 }
 
